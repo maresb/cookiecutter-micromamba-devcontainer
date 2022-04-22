3c185ebb656b2e8176d90db8d1daaa835cd00efb
Ben Mares <services-git-throwaway1@tensorial.com>
2022-04-22 12:27:38 +0200

    Change source_dir validation logic

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,6 +2,10 @@ import sys
 
 source_dir = "{{cookiecutter.source_dir}}"
 
-if source_dir != "" and not source_dir.endswith("/"):
+if source_dir != source_dir.strip():
+    print("ERROR: source_dir contains extraneous whitespace.")
+    sys.exit(1)
+
+if source_dir.startswith("..") or source_dir.startswith("/"):
     print("ERROR: source_dir must be empty or must end with a slash.")
     sys.exit(1)
